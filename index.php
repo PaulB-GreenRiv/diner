f3dca27cadddb697dfc032a107360175a308eebe
Paul Butler <pbutler10@mail.greenriver.edu>
2021-05-11 12:09:35 -0400

    Added extra files and moved the images folder

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//Start a session
+session_start();
+
 //Require autoload file
 require_once ('vendor/autoload.php');
 
@@ -27,6 +30,36 @@ $f3->route('GET /breakfast', function(){
 $f3->route('GET /lunch', function(){
     $view = new Template();
     echo $view->render('views/lunch.html');
+});
+
+$f3->route('GET|POST /order1', function(){
+    //If the form has been submitted, add the data to the session
+    //and send the user to the next order form
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //var_dump($_POST);
+        $_SESSION['food'] = $_POST['food'];
+        $_SESSION['meal'] = $_POST['meal'];
+        header('location: order2');
+    }
+    $view = new Template();
+    echo $view->render('views/orderForm1.html');
+});
+
+$f3->route('GET|POST /order2', function(){
+    //If the form has been submitted, add the data to the session
+    //and send the user to the summary page
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //var_dump($_POST);
+        $_SESSION['conds'] = implode(", ", $_POST['conds']);
+        header('location: summary');
+    }
+    $view = new Template();
+    echo $view->render('views/orderForm2.html');
+});
+
+$f3->route('GET|POST /summary', function(){
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 
 //Run Fat-Free
